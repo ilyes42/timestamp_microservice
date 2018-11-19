dce5d9aabd484925d7b981ea412215bb1335d5ef
Ilyes Hammemi <ilyeshammemi42@gmail.com>
2018-11-19 12:26:39 +0100

    first version

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -20,8 +20,25 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api/timestamp/:date_string?', (req, res) => {
+  if (req.params.date_string) {
+    let d = isNaN(req.params.date_string) ? req.params.date_string : Number(req.params.date_string);
+    let date = new Date(d);
+    if (date.getTime()) {
+      res.json({
+        "unix": date.getTime(),
+        "utc": date.toUTCString()
+      });
+    } else {
+      res.json({ "error": "Invalid Date" });
+    }
+  } else {
+    let date = new Date();
+    res.json({
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    });
+  }
 });
 
 
